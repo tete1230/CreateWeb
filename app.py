f988cd536128d566e8d87847b886ad4cdd20b816
tete1230 <tete1230@163.com>
2024-06-17 21:01:17 +0800

    dd minimal home page

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
-from flask import Flask,request,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/form', methods=['GET', 'POST'])
-def form_example():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        return render_template('hello.html',name=name)
-    return '''
-        <form method="post">
-            Name: <input type="text" name="name">
-            <input type="submit">
-        </form>
-        '''
+    return render_template('index.html', name=name, movies=movies)
 
 
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
+
+
+# @app.route('/home')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/linkeming/Documents/AliServerLocal/MyProject/env"
+set -gx VIRTUAL_ENV "/Users/keming/Documents/AliServer/CreateWeb/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
