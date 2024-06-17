7b443d62d88e4a6563cfcf1a2885c8df75930daf
tete1230 <tete1230@163.com>
2024-06-17 21:01:17 +0800

    dd minimal home page

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
-from flask import Flask,request,render_template
+from flask import Flask
+from flask import url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def hello():
+    return '<h1>Welcome to My 阵地!</h1><img src="http://helloflask.com/totoro.gif">'
 
-@app.route('/form', methods=['GET', 'POST'])
-def form_example():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        return render_template('hello.html',name=name)
-    return '''
-        <form method="post">
-            Name: <input type="text" name="name">
-            <input type="submit">
-        </form>
-        '''
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User:{escape(name)}'
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name='keming'))
+    print(url_for('user_page', name='peter'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for', num=2))
+    return 'Test page'
 
 
 
+# @app.route('/home')
 
 if __name__ == '__main__':
     app.run(debug=True)
